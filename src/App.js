bba873a543209aeaf81138bc46390e8c55994c9f
Muhammad Saad Asghar <muhammadsaadasghar408@gmail.com>
2026-01-04 15:14:49 +0500

    create Navbar and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This  is cloudnotes</h1>
-    </div>
+    <>
+    <Router>
+         <Navbar/>
+  <Routes>
+    <Route path="/home" element={<Home />} />
+    <Route path="/about" element={<About />} />
+  </Routes>
+</Router>
+
+    </>
   );
 }
 
