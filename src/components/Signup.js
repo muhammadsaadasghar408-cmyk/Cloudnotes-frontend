d3bb36b92c686036ce929b3354e5c72b38d123c6
Muhammad Saad Asghar <muhammadsaadasghar408@gmail.com>
2026-01-13 15:47:55 +0500

    adding signup componenet in cloudnotes

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,95 @@
-import React from 'react'
-
+import React,{useState} from "react";
+import {useNavigate} from 'react-router-dom';
 function Signup() {
+   const [credentails, setCredentails] = useState({name:"",email:"",password:"",cpassword:""})
+  let navigate = useNavigate();
+   const handleSubmit= async (e)=>{
+e.preventDefault();
+const {name,email,password}=credentails
+ const response = await fetch("http://localhost:5000/api/auth/createuser", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({name,email,password}),
+});
+const json = await response.json();
+console.log(json)
+if (json.success) {
+  localStorage.setItem('token',json.authtoken);
+    navigate("/");
+}
+else{
+  alert("invalid credentails")
+}
+   }
+   const onChange=(e)=>{
+setCredentails({...credentails,[e.target.name]:e.target.value})
+   }
   return (
-    <div>Signup</div>
-  )
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
+            aria-describedby="emailHelp"
+            onChange={onChange}
+            />
+          
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            aria-describedby="emailHelp"
+            onChange={onChange}
+            />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            onChange={onChange} minLength={5} required
+            />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="cPassword" className="form-label">
+           Confirm Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="cpassword"
+            name="cpassword"
+            onChange={onChange} minLength={5} required
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
